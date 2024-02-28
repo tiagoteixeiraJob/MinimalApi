d48b9aa4e931419eaf1a08956df647f1e41b1efd
tiagoteixeirajob <tiagoteixeirajob@gmail.com>
2024-02-28 11:43:00 +0000

    initial commit

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -5,7 +5,6 @@ using MinimalApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
